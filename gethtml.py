745810acb99f5c98f1bc1a8ad714aaa7c60833cf
ashermanwmf <asherman@wikimedia.org>
2016-01-21 12:29:57 -0500

    first commit

---
+++ gethtml.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import urllib
+import xml.etree.ElementTree as ET
+
+
+for arg in sys.argv[1:]:
+    filename = arg
+
+    requestURL = 'https://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=' + filename
+
+    root = ET.parse(urllib.urlopen(requestURL)).getroot()
+
+    licensehtml= ''
+    authorhtml = ''
+
+    for author in root.iter('author'):
+        authorhtml = author.text
+
+    for licenses in root.findall('licenses'):
+        licensehtml = licenses.find('license').find('name').text
+
+    licensehtml = '<a href="https://creativecommons.org/">' + licensehtml + '</a>'    
+    
+    print '<a href=https://commons.wikimedia.org/wiki/File:' + filename + '>Photo</a> by ' + authorhtml + licensehtml
