97867e49bac42677f7a84344a6e22c472967d920
ashermanwmf <shermanandrew50@gmail.com>
2016-04-06 15:47:05 -0400

    added new styling, links are white and underlined

--- gethtml.py
+++ gethtml.py
@@ -1,37 +1,32 @@
 import os
 import sys
+import re
 import urllib
 import xml.etree.ElementTree as ET
 from BeautifulSoup import BeautifulSoup
 
-cc_0 = 'freely licensed under <a href=" '
-cc_1_0 = 'freely licensed under <a href=" '
-cc_2_0 = 'freely licensed under <a href=" '
-cc_3_0 = 'freely licensed under <a href=" '
-cc_4_0 = 'freely licensed under <a href=" '
-cc_3_0_SA = 'freely licensed under <a href=" '
-cc_4_0_SA = 'freely licensed under <a href=" '
-pd = '<a href=" '
-
+#([\w\s,!*?]*\.\w{3}) regex to use for sys.argv solution (works for anything ending in ".png, .svg, .jpg" etc...
 
 for arg in sys.argv[1:]:
     filename = arg
 
-    requestURL = 'https://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=' + filename
+    requestURL = 'https://tools.wmflabs.org/magnus-toolserver/commonsapi.php?image=' + str(filename) + '&forcehtml'
 
     root = ET.parse(urllib.urlopen(requestURL)).getroot()
 
     licensehtml= ''
     authorhtml = ''
+    soup = ''
 
     for author in root.iter('author'):
         authorhtml = author.text
+        soup = BeautifulSoup(authorhtml)
+        soup.a['style'] = 'color:white;text-decoration:underline;'
 
     for licenses in root.findall('licenses'):
         licensehtml = licenses.find('license').find('name').text
 
-    licensehtml = '<a href="https://creativecommons.org/">' + licensehtml + '</a>'    
-    
-    print '<a href="https://commons.wikimedia.org/wiki/File:' + filename + '">Photo</a> by ' + authorhtml + licensehtml
+    licensehtml = '<a style="color:white;text-decoration:underline;" href="https://creativecommons.org/">' + licensehtml + '</a>'    
+
+    print '<a style="color:white;text-decoration:underline;" href="https://commons.wikimedia.org/wiki/' + filename + '">Photo</a> by ' + str(soup) + ', ' + licensehtml
     print '\n'    
-    
