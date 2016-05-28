bfce7c1ecaa63ddb100529aedb4e21f0ddd9b00a
ashermanwmf <asherman@wikimedia.org>
2016-05-28 10:04:23 -0500

    fixing up last changes

--- getwiki.py
+++ getwiki.py
@@ -1,3 +1,4 @@
+# needs work!!!!!!!
 #########################################################################
 #   How this works                              
 # 1. This script allows for multiple arguments to print mulitple files
@@ -6,7 +7,7 @@
 # 2. The above example returns (still needs appropriate licensing with link):
 #       [[:File:Dog.png|Photo]] by [http://commons.wikimedia.org/wiki/User:Loupeznik Loupeznik], CC-BY-SA-3.0,2.5,2.0,1.0 
 # 3. othe possible example:
-#   ex: $ python getwiki.py File:Dog.png File:Cat.jpg
+#   ex: $ 
 # (choose gethtml.py to print the file info in html format)
 # (one packages: BeautifulSoup)
 #########################################################################
@@ -55,15 +56,16 @@ for arg in sys.argv[1:]:
     author_link = ''
     author_name = ''
     
+    soup = BeautifulSoup(authorhtml)
+
     if is_html(authorhtml) is 'True':
-        soup = BeautifulSoup(authorhtml)
         for author in root.iter('author'):
             authorhtml = author.text
             authorhtml = '[[' + soup.a['title'] + '|' + soup.a.text + ']]'
     else:
         for author in root.iter('author'):
             authorhtml = author.text
-            soup = BeautifulSoup(authorhtml)
+            # currently doesnt work, try to rework this like the get html but just printing everything into wikiformat
             author_link = soup.a['href']
             author_name = soup.a.text
         
@@ -72,3 +74,37 @@ for arg in sys.argv[1:]:
     
     print '[[:' + filename + '|Photo]] by [' + author_link + ' ' + author_name + '], ' + licensehtml
     print '\n'
+
+#FOR LATER USE
+##licensing links (not always in the xml)
+#cc0_1 = ', freely licesned under [https://creativecommons.org/publicdomain/zero/1.0/ '
+#cc_1_0 = ', freely licensed under [http://creativecommons.org/licenses/by/1.0/ '
+#cc_2_0 = ', freely licensed under [http://creativecommons.org/licenses/by/2.0/ '
+#cc_2_5 = ', freely licensed under [http://creativecommons.org/licenses/by/2.5/ '
+#cc_3_0 = ', freely licensed under [http://creativecommons.org/licenses/by/3.0/ '
+#cc_4_0 = ', freely licensed under [http://creativecommons.org/licenses/by/4.0/ '
+#cc_1_0_SA = ', freely licensed under [http://creativecommons.org/licenses/by-sa/1.0/ '
+#cc_2_0_SA = ', freely licensed under [http://creativecommons.org/licenses/by-sa/2.0/ '
+#cc_2_5_SA = ', freely licensed under [http://creativecommons.org/licenses/by-sa/2.5/ '
+#cc_3_0_SA = ', freely licensed under [http://creativecommons.org/licenses/by-sa/3.0/ '
+#cc_4_0_SA = ', freely licensed under [http://creativecommons.org/licenses/by-sa/4.0/ '
+#    if licensehtml == 'CC-Zero':
+#        licensehtml = cc0_1 + licensehtml + ']'
+#    elif licensehtml == 'CC-PD-Mark':
+#        licensehtml = ' ' + licensehtml
+#    elif licensehtml == 'CC-BY-1.0':
+#        licensehtml = cc_1_0 + licensehtml + ']'    
+#    elif licensehtml == 'CC-BY-2.0':
+#        licensehtml = cc_2_0 + licensehtml + ']'
+#    elif licensehtml == 'CC-BY-2.5':
+#        licensehtml = cc_2_5 + licensehtml + ']'
+#    elif licensehtml is 'CC-BY-SA-1.0-migrated' or 'CC-BY-SA-1.0':
+#        licensehtml = cc_1_0_SA + licensehtml + ']'
+#    elif licensehtml is 'CC-BY-SA-2.0-migrated' or 'CC-BY-SA-2.0':
+#        licensehtml = cc_2_0_SA + licensehtml + ']'
+#    elif licensehtml is 'CC-BY-SA-2.5-migrated' or 'CC-BY-SA-2.5':
+#        licensehtml = cc_2_5_SA + licensehtml + ']'
+#    elif licensehtml is 'CC-BY-SA-3.0-migrated' or 'CC-BY-SA-3.0':
+#        licensehtml = cc_3_0_SA + licensehtml + ']'
+#    elif licensehtml is 'CC-BY-SA-4.0-migrated' or 'CC-BY-SA-4.0':
+#        licensehtml = cc_4_0_SA + licensehtml + ']'
